bee90841acc577de2a225a1295fdbebb778af58f
kartikey-udainiya <kartikeyudainiya@gmail.com>
2025-11-26 21:54:48 +0530

    rate limiting for auth implemented

---
+++ src/middleware/rateLimiter.js
@@ -0,0 +1,13 @@
+import rateLimit from "express-rate-limit";
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, 
+  message: {
+    success: false,
+    message: "Too many attempts, please try again later."
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+export default authLimiter;
